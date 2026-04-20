a385d86d2977b39f017d170723c91634f35fb91a
i0871601 <i0871601@gmail.com>
2026-04-20 12:22:20 +0300

    Update map_data.js

--- map_data.js
+++ map_data.js
@@ -82,8 +82,8 @@ const RENDER_MAP = {
         const ratio = width / height;
 
         // Збільшив діапазон для кращого вигляду
-        const minCells = 11;
-        const maxCells = 15;
+        const minCells = 7;
+        const maxCells = 9;
 
         let bestX = 1, bestY = 1;
         let minDifference = Infinity;
